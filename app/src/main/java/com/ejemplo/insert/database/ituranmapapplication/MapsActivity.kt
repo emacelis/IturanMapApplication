dc70fc87427cd17efd0fc6f28794dc06333192ab
Emmanuel Celis <40809233+emacelis@users.noreply.github.com>
2020-03-14 21:46:53 -0600

    Se adapta la clase Map para obtener la ubicación del cell Se añade la clase inicializarLocationRequest para especificar los parametros de la ubicación

--- app/src/main/java/com/ejemplo/insert/database/ituranmapapplication/MapsActivity.kt
+++ app/src/main/java/com/ejemplo/insert/database/ituranmapapplication/MapsActivity.kt
@@ -2,6 +2,11 @@ package com.ejemplo.insert.database.ituranmapapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,26 +17,52 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    //VARIABLES PARA LA UBICACION
+    private val permisoFineLocation=android.Manifest.permission.ACCESS_FINE_LOCATION
+    private val permisoCoarseLocation=android.Manifest.permission.ACCESS_FINE_LOCATION
+    private val CODIGO_SOLICITUD_PERMISO=100 //Codigo de permiso
+    var fusedLocationClient: FusedLocationProviderClient?=null//permiso para pedir la Latitud
+    var locationRequest: LocationRequest?=null
+    var callback: LocationCallback?=null
+
+
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient= FusedLocationProviderClient(this)
+        inicializarLocationRequest()
+
+        callback=object :LocationCallback(){
+
+            override fun onLocationResult(locationResoult: LocationResult?) {
+                super.onLocationResult(locationResoult)//location resoult entregar un objeto que tiene un arreglo de las ubcicaciones mas recientes
+
+                for (ubicacion in locationResoult?.locations!!){
+                    Toast.makeText(applicationContext,ubicacion.latitude.toString()+","+ubicacion.longitude.toString(),
+                        Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    //Se tiene que configurtar los parametros de actualizacion
+    private fun inicializarLocationRequest(){
+        locationRequest= LocationRequest()
+        locationRequest?.interval=1000//intervalo de actualizacion
+        locationRequest?.fastestInterval=5000//rango maximo de tiempo
+        locationRequest?.priority=LocationRequest.PRIORITY_HIGH_ACCURACY//10m de variacion
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
